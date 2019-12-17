1c689224339879a4f40b91b31d65062d34586c6d
Neil John Medalla <medallaneiljohn@gmail.com>
2019-12-16 22:19:26 -0700

    second commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,13 +18,13 @@ export const NavBar = () => {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mx-auto">
-          <Link to="/"><Nav.Link href="#home">HOME</Nav.Link></Link>
-          <Link to="/beers"><Nav.Link href="#beers">BEERS</Nav.Link></Link>
-          <Link to="/tasting/room"><Nav.Link href="#tasting-room">TASTING ROOM</Nav.Link></Link>
-          <Link to="/find/us"><Nav.Link href="#find-us">WHERE TO FIND US</Nav.Link></Link>
-          <Link to="/artist/residence"><Nav.Link href="#artist-in-residence">ARTIST IN RESIDENCE</Nav.Link></Link>
-          <Link to="/story"><Nav.Link href="#story">STORY</Nav.Link></Link>
-          <Link to="/contact"><Nav.Link href="#contact-us">CONTACT US</Nav.Link></Link>
+          <Link to="/" className="nav-link"><Nav.Link href="#home">HOME</Nav.Link></Link>
+          <Link to="/beers" className="nav-link"><Nav.Link href="#beers">BEERS</Nav.Link></Link>
+          <Link to="/tasting/room" className="nav-link"><Nav.Link href="#tasting-room">TASTING ROOM</Nav.Link></Link>
+          <Link to="/find/us" className="nav-link"><Nav.Link href="#find-us">WHERE TO FIND US</Nav.Link></Link>
+          <Link to="/artist/residence" className="nav-link"><Nav.Link href="#artist-in-residence">ARTIST IN RESIDENCE</Nav.Link></Link>
+          <Link to="/story" className="nav-link"><Nav.Link href="#story">STORY</Nav.Link></Link>
+          <Link to="/contact" className="nav-link"><Nav.Link href="#contact-us">CONTACT US</Nav.Link></Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
